b00cae084b284b49421e79e37295a22423e0941a
LENA EIVY <applena@gmail.com>
2019-02-21 16:42:51 -0800

    it works locally

--- src/components/header.js
+++
@@ -1,3 +0,0 @@
-import React from "react"
-
-export default (props) => <h1 style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>{props.headerText}</h1>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Layout from '../components/layout';
+import me from '../../static/img/me.jpg';
+import styles from "./index.module.css";
 
 export default () => (
   <Layout>
     <div style={{ color: `purple` }}>
-      <p>I like to do lots of things, but mostly build website.</p>
-      <p>I like pretty pictures.</p>
-      <img src="https://source.unsplash.com/random/400x200" alt="" />
+      <h2>Lena Eivy</h2>
+      <p>I like to do lots of things, but mostly build websites.</p>
+      <p>this is me</p>
+      <img className={styles.img} src={me} alt="" />
     </div>
   </Layout>
 )
